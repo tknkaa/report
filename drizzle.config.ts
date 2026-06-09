63a19c33f9dffa2a04325b7991bbc4a24194ed37
tknkaa <tknkaa@users.noreply.github.com>
2026-06-10 00:19:49 +0900

    Update drizzle.config.ts and worker-configuration.d.ts for D1 database integration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
+  driver: "d1-http",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+    token: process.env.CLOUDFLARE_D1_TOKEN,
   },
 })
